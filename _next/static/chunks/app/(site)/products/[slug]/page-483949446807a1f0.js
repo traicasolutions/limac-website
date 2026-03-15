67f933d3fa0f1cfbfbe067dbd6a72582609ab4b6
dileepvarier <dileepvarier@users.noreply.github.com>
2026-03-15 15:00:57 +0000

    deploy: 88c2be208aee1eed191ebc00f539dd2576c65fe6

---
+++ _next/static/chunks/app/(site)/products/[slug]/page-483949446807a1f0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[860],{226:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,3513))}},function(e){e.O(0,[972,878,877,513,971,117,744],function(){return e(e.s=226)}),_N_E=e.O()}]);
